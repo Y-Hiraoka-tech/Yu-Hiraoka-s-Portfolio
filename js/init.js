f447dfbc0db381dc262fe98de87d488fef782ef3
Yuhiraoka <hirayu955@gmail.com>
2021-11-22 13:41:24 +0900

    ポートフォリオ完成

--- js/init.js
+++ js/init.js
@@ -394,7 +394,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Life Saver", "Swimming Shool Coach", "University Student"],
+			strings: ["Lifesaver", "Swimming Shool Coach", "University Student"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
